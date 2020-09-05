f6399d3c9bb35ee43855cdd5e7d392e077e3c8e9
Yash Walia <yashbest005@gmail.com>
2020-09-05 21:07:38 +0530

    Updates

---
+++ precache-manifest.459a43f5a3fa357a795fae3ac276d482.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c48decba60ee3bebd6c655679202643",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "64b4692ff3b2105bde75",
+    "url": "/todolist-react/static/css/main.6f24de38.chunk.css"
+  },
+  {
+    "revision": "67361699d8ca39151a38",
+    "url": "/todolist-react/static/js/2.25e11dd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-react/static/js/2.25e11dd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b4692ff3b2105bde75",
+    "url": "/todolist-react/static/js/main.7d36701b.chunk.js"
+  },
+  {
+    "revision": "87f912004f3c9790f7f8",
+    "url": "/todolist-react/static/js/runtime-main.59b16a4f.js"
+  },
+  {
+    "revision": "00a82dfc0995baa5b23e1502b8f108cf",
+    "url": "/todolist-react/static/media/back.00a82dfc.jpg"
+  }
+]);
